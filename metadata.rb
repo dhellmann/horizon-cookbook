6d30b49a2492db2c981f1f5159d9b76d9e85093b
mattray <matthewhray@gmail.com>
2012-06-28 16:43:59 -0700

    moved version to 5.0.0 for Essex

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
-maintainer       "Rackspace Hosting, Inc."
+maintainer       "Opscode, Inc."
+maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
-description      "Installs/Configures horizon"
+description      "The OpenStack Dashboard service Horizon."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "5.0.0"
 
 %w{ ubuntu }.each do |os|
   supports os
