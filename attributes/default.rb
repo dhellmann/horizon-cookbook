cfba17b4222414b8a7b47913fa4d8204e3d5fa36
Carl Perry <carl.perry@dreamhost.com>
2012-11-15 08:18:59 -0800

    Missing attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,8 +6,8 @@ default["developer_mode"] = false  # we want secure passwords by default
 
 default["openstack"]["horizon"]["folsom"]["version"] = "2012.2+git201209242000~precise-0ubuntu1"
 
-default["openstack"]["horizon"]["db"]["name"] = "nova"
-default["openstack"]["horizon"]["db"]["username"] = "nova"
+default["openstack"]["horizon"]["db"]["name"] = "horizon"
+default["openstack"]["horizon"]["db"]["username"] = "horizon"
 
 # Account for the moving target that is the dashboard path....
 default["openstack"]["horizon"]["dash_path"] = "/usr/share/openstack-dashboard/openstack_dashboard"
